27a009bce1b63b22ca7412c149b6f7ca5ff824ac
Gr4L22 <umarinzalda@gmail.com>
2020-11-02 12:13:05 -0300

    minimal styles changes

--- js/main.js
+++ js/main.js
@@ -31,157 +31,196 @@ var errorID = document.getElementById("errorID");
 ////////////////////////////////////////////////////////////////////////////////////////
 var button = document.getElementById("button");
 var data = {};
-var dataError = ['' , '' , '' , '' , '' , '' , '' , '' , '' , '' , 'Empty form'];
+var dataError = ["", "", "", "", "", "", "", "", "", "", "Empty form"];
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-function logicName(e){
-    if (nameVerification(e.target.value)){
-        data.name=e.target.value;
-    } else {
-        data.name=undefined;
-        errorName.style.display = 'block';
-        dataError[0]='Name Error';
-        dataError[10]=' ';
-    }
+function logicName(e) {
+  if (nameVerification(e.target.value)) {
+    data.name = e.target.value;
+  } else {
+    data.name = undefined;
+    errorName.style.display = "block";
+    dataError[0] = "Name Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorName() {
-    errorName.style.display = 'none';
+  errorName.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicMail(e) {
-    if (mailVerification(e.target.value)) {
-        data.email=e.target.value;
-    } else {
-        data.email=undefined;
-        errorEmail.style.display = 'block';
-        dataError[1]='Mail Error';
-        dataError[10]=' ';
-    }
+  if (mailVerification(e.target.value)) {
+    data.email = e.target.value;
+  } else {
+    data.email = undefined;
+    errorEmail.style.display = "block";
+    dataError[1] = "Mail Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorMail() {
-    errorEmail.style.display = 'none';
+  errorEmail.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicPass(e) {
-    if (passVerification(e.target.value)) {
-        data.pass=e.target.value;
-    } else {
-        data.pass=undefined;
-        errorPassword.style.display = 'block';
-        dataError[2]='Pass Error';
-        dataError[10]=' ';
-    }
+  if (passVerification(e.target.value)) {
+    data.pass = e.target.value;
+  } else {
+    data.pass = undefined;
+    errorPassword.style.display = "block";
+    dataError[2] = "Pass Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorPass() {
-    errorPassword.style.display = 'none';
+  errorPassword.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicRePass(e) {
-    if (data.pass == e.target.value) {
-        data.rePass=e.target.value;
-    } else {
-        data.rePass=undefined;
-        errorRePassword.style.display = 'block';
-        dataError[3]='Re Pass Error';
-        dataError[10]=' ';
-    }
+  if (data.pass == e.target.value) {
+    data.rePass = e.target.value;
+  } else {
+    data.rePass = undefined;
+    errorRePassword.style.display = "block";
+    dataError[3] = "Re Pass Error";
+    dataError[10] = " ";
+  }
 }
 function displayReErrorPass() {
-    errorRePassword.style.display = 'none';
+  errorRePassword.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicAge(e) {
-    if (ageVerification(e.target.value)) {
-        data.age=e.target.value;
-    } else {
-        data.age=undefined;
-        errorAge.style.display = 'block';
-        dataError[4]='Age Error';
-        dataError[10]=' ';
-    }
+  if (ageVerification(e.target.value)) {
+    data.age = e.target.value;
+  } else {
+    data.age = undefined;
+    errorAge.style.display = "block";
+    dataError[4] = "Age Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorAge() {
-    errorAge.style.display = 'none';
+  errorAge.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicPhone(e) {
-    if (phoneVerification(e.target.value)) {
-        data.phone=e.target.value;
-    } else {
-        data.phone=undefined;
-        errorPhone.style.display = 'block';
-        dataError[5]='Phone Error';
-        dataError[10]=' ';
-    }
+  if (phoneVerification(e.target.value)) {
+    data.phone = e.target.value;
+  } else {
+    data.phone = undefined;
+    errorPhone.style.display = "block";
+    dataError[5] = "Phone Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorPhone() {
-    errorPhone.style.display = 'none';
+  errorPhone.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicDirec(e) {
-    if (direcVerification(e.target.value)) {
-        data.direc=e.target.value;
-    } else {
-        data.direc=undefined;
-        errorDirec.style.display = 'block';
-        dataError[6]='Direction Error';
-        dataError[10]=' ';
-    }
+  if (direcVerification(e.target.value)) {
+    data.direc = e.target.value;
+  } else {
+    data.direc = undefined;
+    errorDirec.style.display = "block";
+    dataError[6] = "Direction Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorDirec() {
-    errorDirec.style.display = 'none';
+  errorDirec.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicCity(e) {
-    if (minLength(e.target.value,3)) {
-        data.city=e.target.value;
-    } else {
-        data.city=undefined;
-        errorCity.style.display = 'block';
-        dataError[7]='City Error';
-        dataError[10]=' ';
-    }
+  if (minLength(e.target.value, 3)) {
+    data.city = e.target.value;
+  } else {
+    data.city = undefined;
+    errorCity.style.display = "block";
+    dataError[7] = "City Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorCity() {
-    errorCity.style.display = 'none';
+  errorCity.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicPostCode(e) {
-    if (minLength(e.target.value,3)) {
-        data.postCode=e.target.value;
-    } else {
-        data.postCode=undefined;
-        errorPost.style.display = 'block';
-        dataError[8]='Post Code Error';
-        dataError[10]=' ';
-    }
+  if (minLength(e.target.value, 3)) {
+    data.postCode = e.target.value;
+  } else {
+    data.postCode = undefined;
+    errorPost.style.display = "block";
+    dataError[8] = "Post Code Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorPost() {
-    errorPost.style.display = 'none';
+  errorPost.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 function logicID(e) {
-    if (idVerification(e.target.value)) {
-        data.id=e.target.value;
-    } else {
-        data.id=undefined;
-        errorID.style.display = 'block';
-        dataError[9]='ID Error';
-        dataError[10]=' ';
-    }
+  if (idVerification(e.target.value)) {
+    data.id = e.target.value;
+  } else {
+    data.id = undefined;
+    errorID.style.display = "block";
+    dataError[9] = "ID Error";
+    dataError[10] = " ";
+  }
 }
 function displayErrorID() {
-    errorID.style.display = 'none';
+  errorID.style.display = "none";
 }
 ////////////////////////////////////////////////////////////////////////////////////////
-function printAlert (e) {
-    e.preventDefault;
-    if(dataValidator(data)) {
-        alert(data.email+' '+data.name+' '+data.age+' '+data.phone+' '+data.direc+' '+data.city+' '+data.postCode+' '+data.id);
-    } else {
-        alert('There is an error in the form: '+dataError[0]+' '+dataError[1]+' '+dataError[2]+' '+dataError[3]+' '+dataError[4]+' '+dataError[5]+' '+dataError[6]+' '+dataError[7]+' '+dataError[8]+' '+dataError[9]+' '+dataError[10]);
-    }
+function printAlert(e) {
+  e.preventDefault;
+  if (dataValidator(data)) {
+    alert(
+      data.email +
+        " " +
+        data.name +
+        " " +
+        data.age +
+        " " +
+        data.phone +
+        " " +
+        data.direc +
+        " " +
+        data.city +
+        " " +
+        data.postCode +
+        " " +
+        data.id
+    );
+  } else {
+    alert(
+      "There is an error in the form: " +
+        dataError[10] +
+        " " +
+        dataError[0] +
+        " " +
+        dataError[1] +
+        " " +
+        dataError[2] +
+        " " +
+        dataError[3] +
+        " " +
+        dataError[4] +
+        " " +
+        dataError[5] +
+        " " +
+        dataError[6] +
+        " " +
+        dataError[7] +
+        " " +
+        dataError[8] +
+        " " +
+        dataError[9]
+    );
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 fullName.addEventListener("blur", logicName);
